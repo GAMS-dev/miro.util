48c863a6b65a2b05b38771fbc269fe1c8d91ce3b
Frederik Proske <fproske@gams.com>
2022-05-24 15:12:15 +0200

    Migrate Rcpp to cpp11

---
+++ R/miroUtil-package.R
@@ -0,0 +1,4 @@
+## usethis namespace: start
+#' @useDynLib miroUtil, .registration = TRUE
+## usethis namespace: end
+NULL
--- src/interrupt-gams-win.cpp
+++ src/interrupt-gams-win.cpp
@@ -1,39 +1,23 @@
-#include <Rcpp.h>
-#ifdef __MINGW32__
-// Taken from http://tolstoy.newcastle.edu.au/R/e2/devel/06/11/1242.html
-// Undefine the Realloc macro, which is defined by both R and by Windows stuff
-#undef Realloc
-// Also need to undefine the Free macro
-#undef Free
-
+#ifdef _WIN32
 #include <windows.h>
 
 using namespace std;
 #include <string>
 #include <sstream>
-// workaround to make std::to_string work from:
-// https://stackoverflow.com/questions/12975341/to-string-is-not-a-member-of-std-says-g-mingw
-namespace patch
-{
-    template < typename T > std::string to_string( const T& n )
-    {
-        std::ostringstream stm ;
-        stm << n ;
-        return stm.str() ;
-    }
-}
 #endif
-using namespace Rcpp;
 
-// [[Rcpp::export]]
+#include "cpp11.hpp"
+using namespace cpp11;
+
+[[cpp11::register]]
 int windowsInterruptGAMS(int pid)
 {
-#ifdef __MINGW32__
+#ifdef _WIN32
     COPYDATASTRUCT cds;
     const char* msgText = "GAMS Message Interrupt";
 
     std::string windowName = "___GAMSMSGWINDOW___";
-    windowName += patch::to_string(pid);
+    windowName += std::to_string(pid);
     HWND receiver = FindWindowA(NULL, windowName.c_str());
     if (!receiver) { return 0; }
 
